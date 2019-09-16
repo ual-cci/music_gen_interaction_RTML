0e32c0b2781239ecf5a50e6a726acfacccfda804
previtus <previtus@gmail.com>
2019-09-16 21:44:47 +0100

    A working example (with bits of hardcoded generation code - this will be later split in between the model handler and an audio handler)

--- model_handler_lstm.py
+++ model_handler_lstm.py
@@ -3,7 +3,7 @@
 import numpy as np
 import tensorflow as tf
 import tflearn
-from tflearn.layers.recurrent import bidirectional_rnn, BasicLSTMCell, GRUCell
+from tflearn.layers.recurrent import bidirectional_rnn, BasicLSTMCell
 from tflearn.layers.core import dropout
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from utils.audio_dataset_generator import AudioDatasetGenerator
@@ -125,9 +125,82 @@ class ModelHandlerLSTM(object):
 
         self.model = model
 
+    def generate(self, dataset, amount_samples = 8, sequence_length_max = 700):
+        # Dataset
+        self.use_wavelets = False
+        self.wavelet = 'db10'
+        self.window_size = 1024
+
+
+        impulse_scale = 1.0
+        griffin_iterations = 60
+        random_chance = 0.8
+        random_strength = 0.8
+
+        dimension1 = self.input_shapes[1]
+        dimension2 = self.input_shapes[2]
+        shape = (1, dimension1, dimension2, 1) if self.use_cnn else (1, dimension1, dimension2)
+
+        audio = []
+        impulses = []
+
+        if self.use_wavelets:
+            temp_audio = np.array(0)
+        for i in range(amount_samples):
+
+            random_index = np.random.randint(0, (len(dataset.x_frames) - 1))
+            impulse = np.array(dataset.x_frames[random_index]) * impulse_scale
+            predicted_magnitudes = impulse
+
+            if self.use_wavelets:
+                for seq in range(impulse.shape[0]):
+                    coeffs = pywt.array_to_coeffs(impulse[seq], dataset.coeff_slices)
+                    recon = (pywt.waverecn(coeffs, wavelet=self.wavelet))
+                    temp_audio = np.append(temp_audio, recon)
+            for j in range(sequence_length_max):
+                prediction = model.predict(impulse.reshape(shape))
+
+                ##impulses += [impulse] ## << mem issues
+
+                # Wavelet audio
+                if self.use_wavelets:
+                    coeffs = pywt.array_to_coeffs(prediction[0], dataset.coeff_slices)
+                    recon = (pywt.waverecn(coeffs, wavelet=self.wavelet))
+                    temp_audio = np.append(temp_audio, recon)
+
+                if self.use_cnn:
+                    prediction = prediction.reshape(1, dataset.y_frames.shape[1], 1)
+
+                # print("prediction.shape", prediction.shape)
+                # prediction.shape (1, 1025)
+
+                ### add the last prediction to the predicted_magnitudes
+                predicted_magnitudes = np.vstack((predicted_magnitudes, prediction))
+                impulse = predicted_magnitudes[-self.sequence_length:]
+
+                # print("impulse.shape", impulse.shape)
+
+                if (np.random.random_sample() < random_chance):
+                    idx = np.random.randint(0, dataset.sequence_length)
+                    impulse[idx] = impulse[idx] + np.random.random_sample(impulse[idx].shape) * random_strength
+
+                done = int(float(i * sequence_length_max + j) / float(amount_samples * sequence_length_max) * 100.0) + 1
+                sys.stdout.write('{}% audio generation complete.   \r'.format(done))
+                sys.stdout.flush()
+
+            if self.use_wavelets:
+                audio += [temp_audio]
+            else:
+                predicted_magnitudes = np.array(predicted_magnitudes).reshape(-1, self.window_size + 1)
+                # print("predicted_magnitudes.shape", predicted_magnitudes.shape)
+                audio += [dataset.griffin_lim(predicted_magnitudes.T, griffin_iterations)]
+
+        audio = np.array(audio)
+        return audio
+
 
 # Example calls and outputs:
-"""
+#"""
 test_handler = ModelHandlerLSTM()
 test_handler.create_model()
 
@@ -137,9 +210,27 @@ print(model.net)
 print(model.inputs)
 print(model.targets)
 
-
 #<tflearn.models.dnn.DNN object at 0x7f8938542e80>
 #Tensor("FullyConnected_1/BiasAdd:0", shape=(?, 1025), dtype=float32)
 #[<tf.Tensor 'input_data0/X:0' shape=(?, 40, 1025) dtype=float32>]
 #[<tf.Tensor 'TargetsData/Y:0' shape=(?, 1025) dtype=float32>]
-"""
+#"""
+
+# Second part of the example ...
+from utils.audio_dataset_generator import AudioDatasetGenerator
+import librosa
+
+dataset = AudioDatasetGenerator(sequence_length=40)
+print("loading dataset from a wav file")
+audio_data_path = "/media/vitek/Data/Vitek/Projects/2019_LONDON/music generation/small_file/"
+#dataset.load(audio_data_path, force=True, prevent_shuffling=False)
+dataset.load(audio_data_path, force=True, prevent_shuffling=True)
+
+audio = test_handler.generate(dataset, amount_samples = 2)
+
+print("audio generated, saving")
+print("audio", audio.shape)
+
+for sample_i, sample in enumerate(audio):
+    sample_rate = 44100
+    librosa.output.write_wav('data/sample_i-'+str(sample_i)+'.wav', audio[sample_i], sample_rate)
