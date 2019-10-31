947c8571adc2915a9f23bb3e638a3669bfa95d58
previtus <previtus@gmail.com>
2019-10-31 20:21:45 +0000

    data added

--- cooked_files_handler.py
+++ cooked_files_handler.py
@@ -15,7 +15,8 @@ class CookedFilesHandler(object):
         model_path_start = self.settings.server_model_paths_start
         model_paths = ["modelBest_DNB1.tfl", "modelBest_LPAmbient.tfl", "modelBest_Mehldau.tfl", "modelBest_Ambient.tfl",
                        "modelBest_Dungeon.tfl", "modelBest_Orchestral.tfl", "modelBest_Sneak.tfl",
-                       "modelBest_Glass1.tfl", "modelBest_Glass2.tfl", "modelBest_Twinpeaks.tfl" ]
+                       "modelBest_Glass1.tfl", "modelBest_Glass2.tfl", "modelBest_Twinpeaks.tfl",
+                       "modelBest_Providence.tfl"]
         self.model_paths = [model_path_start+p for p in model_paths]
 
         # Loading from WAVs will be slow ... maybe load from the NPY's directly?
@@ -23,7 +24,7 @@ class CookedFilesHandler(object):
         song_paths_start = self.settings.server_songs_paths_start
         song_paths = ["dnb", "lp", "mehldau", "ambience", "dungeon","orchestral", "sneak", "glass1", "glass2", "twinpeaks"]
 
-        self.names_for_debug = song_paths
+        self.names_for_debug = song_paths + ["providence"]
 
         self.song_paths = [song_paths_start+s+"/" for s in song_paths]
 
