3e46e61c2eb6cad4af4ae70d513bb408718d19bf
previtus <previtus@gmail.com>
2019-11-08 15:17:28 +0000

    better restart

--- demo.py
+++ demo.py
@@ -4,6 +4,8 @@ from multiprocessing import Pool
 import subprocess
 subprocess.run(["killall", "jackd"])
 subprocess.run(["pkill", "-f", "client__playbackWithServer.py"]) # prevent frozen instances!
+subprocess.run(["pkill", "-f", "server.py"]) # prevent frozen instances!
+subprocess.run(["pkill", "-f", "osc_interaction.py"]) # prevent frozen instances!
 subprocess.Popen(["jackd", "-R", "-d", "alsa", "-r", "44100"]) # start jackd with specific rate 44.1kHz
 
 processes = ('server.py', 'osc_interaction.py', 'client__playbackWithServer.py')
